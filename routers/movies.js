bbb344257c960c96b5e36cd72914af9d2a1dd3a7
sefi.uzan <sefi.uzan@overwolf.com>
2022-04-02 17:51:02 +0300

    added protection to movies api

--- routers/movies.js
+++ routers/movies.js
@@ -11,18 +11,18 @@ const { getMovies, createMovie, updateMovie, deleteMovie, getMovie } = require('
 const { protect } = require('../middleware/auth')
 
 // GET request to /api/movies
-router.route('/').get(getMovies)
+router.route('/').get(protect, getMovies)
 
 // POST request to /api/movies/create
-router.route('/create').post(createMovie)
+router.route('/create').post(protect, createMovie)
 
 // PUT request to /api/movies/:name
-router.route('/:movieName').put(updateMovie)
+router.route('/:movieName').put(protect, updateMovie)
 
 // DELETE request to /api/movies/:name
-router.route('/:movieName').delete(deleteMovie)
+router.route('/:movieName').delete(protect, deleteMovie)
 
 // GET request to /api/movies/:name
-router.route('/:movieName').put(getMovie)
+router.route('/:movieName').put(protect, getMovie)
 
 module.exports = router
